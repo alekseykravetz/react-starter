9dea9adebbdb4e7b776b4ef5b2eb8ea6a7d7b4ae
Aleksey Kravetz <alex@alekseys-mbp.mynet>
2019-06-16 15:00:34 +0300

    INIT: React, Webpack, Babel, Eslint

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React Webpack Babel Setupsdfdsfdsfsdfdfsdfsfsfsfsfsdfdfsfsfsdfsdfssfsdfdsfsdf';
+
+ReactDOM.render(
+    <div>{title}</div>,
+    document.getElementById('app'),
+);
+
+// to disable browser auto reloading
+// module.hot.accept();
