ca853da0c8a5d123eb9f62af18e6d8c774803f55
Aleksey Kravetz <alex@alekseys-mbp.mynet>
2019-11-04 16:26:21 +0200

    added TextInput control

--- src/components/Home.component.js
+++ src/components/Home.component.js
@@ -2,40 +2,42 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 // import { observer } from 'mobx-react';
 
 import store from '../store';
 
+import TextInput from './controls/TextInput.control.component';
+
+
 
 const Home = props => {
+    const classes = useStyles();
 
     return (
         <React.Fragment>
 
-            <div className="home-title">
-                <span>Home Route</span>
+            <div className={classes.Title}>
+                <span className={classes.TitleText}>Home Route</span>
             </div>
 
-            <div className="home-content">
+            <div className={classes.Content}>
 
-                <div className="app-vertical-container-example">
+                <div className={classes.verticalContainer}>
                     Counter: {store.counter}
                 </div>
 
-                <div className="app-vertical-container-example" onClick={() => props.history.push('/about')}>navigate to About Page</div>
-                <div className="app-vertical-container-example">
+                <div className={classes.verticalContainer}
+                    onClick={() => props.history.push('/about')}>
+                    navigate to About Page
+                </div>
+                <div className={classes.verticalContainer}>
                     <Link to="/about">About</Link>
                 </div>
 
-                <div className="app-vertical-container-example">
-                    <div className="app-control">
-                        <div className="app-control-label">Input Label</div>
-                        <input type="text" value={store.someInput} onChange={e => store.updateSomeInputProperty(e.target.value)} />
-                    </div>
-                    <div>
-                        Input Value: {store.someInput}
-                    </div>
+                <div className={classes.verticalContainer}>
+                    <TextInput label="Input label" />
                 </div>
 
 
@@ -43,17 +45,17 @@ const Home = props => {
                     Hello World
                 </Button>
 
-                <div className="home-content-buttons">
+                <div className={classes.Buttons}>
                     <div className="app-button secondary" onClick={() => console.log('Back button clicked')}>Back</div>
                     <div className="app-button primary" onClick={() => console.log('Next button clicked')}>Next</div>
                 </div>
 
-                <div className="home-content-buttons">
+                <div className={classes.Buttons}>
                     <div className={`app-button secondary  ${false ? '' : 'disabled'}`} onClick={() => false && console.log('Back button clicked')}>Back</div>
                     <div className={`app-button primary  ${false ? '' : 'disabled'}`} onClick={() => false && console.log('Next button clicked')}>Next</div>
                 </div>
 
-                <div className="home-content-buttons">
+                <div className={classes.Buttons}>
                     <div className="app-button primary" onClick={store.signUp}>Signup</div>
                     <div className="app-button primary" onClick={store.signIn}>Signin</div>
                 </div>
@@ -65,3 +67,32 @@ const Home = props => {
 };
 
 export default Home;
+
+const useStyles = makeStyles(theme => ({
+    verticalContainer: {
+        padding: '5px 0',
+    },
+    Title: {
+        padding: 10,
+        borderRadius: '6px 6px 0 0',
+        boxhsadow: '0 1px 2px rgba(0, 0, 0, 0.16)',
+        backgroundColor: '#6b6b6b',
+    },
+    TitleText: {
+        marginLeft: 20,
+        color: '#61f4ff',
+        fontSize: 19,
+        fontWeight: 500,
+    },
+
+    Content: {
+        padding: 20
+    },
+    Buttons: {
+        display: 'flex',
+        userSelect: 'none',
+        justifyContent: 'flex-end',
+        marginTop: 20,
+        marginiRght: 20,
+    },
+}));
---
+++ src/components/controls/TextInput.control.component.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const TextInput = ({label}) => {
+    const classes = useStyles();
+    const [value, setValue] = React.useState('');
+
+    return (
+        <>
+            <div className={classes.control}>
+                <div className={classes.label}>{label}</div>
+                <input className={classes.input} type="text" value={value} onChange={e => setValue(e.target.value)} />
+            </div>
+            <div>
+                Input Value: {value}
+            </div>
+        </>
+    );
+};
+
+export default TextInput;
+
+
+const useStyles = makeStyles(theme => ({
+    control: {
+        maxWidth: 200
+    },
+    label: {
+        marginTop: 5,
+        color: '#282828',
+        fontSize: 14,
+        fontWeight: 500
+    },
+    input: {
+        background: 'none',
+        border: '1px solid #8e8e8e',
+        borderRadius: 6,
+        fontSize: 14,
+        color: '#282828',
+        outline: 'none',
+        padding: '5px 10px',
+        whitepsace: 'nowrap',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+    },
+
+}));
+
